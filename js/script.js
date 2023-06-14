b16bd21e6fa128b705ed69a466636920c7786063
Andrei Peryshkov <121563808+AndreyP30@users.noreply.github.com>
2023-06-14 15:34:44 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const headerNav = document.querySelector('.header-nav')
+
+hamburgerMenu.addEventListener('click', function(){
+    this.classList.toggle('active');
+    headerNav.classList.toggle('header-nav--active');
+});
